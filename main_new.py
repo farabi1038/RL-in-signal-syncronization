4b08bf25b1bbfdf675b47263bc99e19bae92087e
Ibne Farabi Shihab <ibnfarabishihab@gmail.com>
2023-08-03 10:13:53 -0500

    working one

--- main_new.py
+++ main_new.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-import gym
-from stable_baselines3.dqn.dqn import DQN
+import gymnasium as gym
+from stable_baselines3 import DQN
 
 
-if "SUMO_HOME" in os.environ:
-    tools = os.path.join(os.environ["SUMO_HOME"], "tools")
-    sys.path.append(tools)
-else:
-    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+# if "SUMO_HOME" in os.environ:
+#     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+#     sys.path.append(tools)
+# else:
+#     sys.exit("Please declare the environment variable 'SUMO_HOME'")
 import traci
 
 from sumo_rl import SumoEnvironment
@@ -17,8 +17,8 @@ from sumo_rl import SumoEnvironment
 
 if __name__ == "__main__":
     env = SumoEnvironment(
-        net_file='/Users/ibnefarabishihab/Documents/GitHub/RL-in-signal-syncronization/sumo_files/foothill.net.xml',
-                route_file='/Users/ibnefarabishihab/Documents/GitHub/RL-in-signal-syncronization/sumo_files/route.sample.xml',
+        net_file='/Users/user/Documents/GitHub/RL-in-signal-syncronization/sumo_files/foothill.net.xml',
+                route_file='/Users/user/Documents/GitHub/RL-in-signal-syncronization/sumo_files/route.sample.xml',
                 out_csv_name='path_to_output.csv',
                 single_agent=True,
                 use_gui=True,
